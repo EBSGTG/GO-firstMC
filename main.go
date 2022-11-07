a3abf3d1bc1bb7074777eff3e5ab2abacfbe1a00
EBSGTG <gerty438@gmail.com>
2022-11-07 22:48:37 +0200

    срав та мазав

--- main.go
+++ main.go
@@ -1,46 +1,15 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"log"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/cats/:data", GetCats)
-	e.POST("/cats", AddCat)
-	e.Use(middleware.Logger())
-	e.Use(serverHeader)
-	e.Logger.Fatal(e.Start(":8000"))
-
-}
-func GetCats(c echo.Context) error {
-	catName := c.QueryParam("name")
-	catType := c.QueryParam("type")
-	return c.String(http.StatusOK, fmt.Sprintf("your cat name is : %s\nand cat type is :%s\n", catName, catType))
-}
-func AddCat(c echo.Context) error {
-	type Cat struct {
-		Name string `json:"name"`
-		Type string `json:"type"`
-	}
-	cat := Cat{}
-	defer c.Request().Body.Close()
-	err := json.NewDecoder(c.Request().Body).Decode(&cat)
-	if err != nil {
-		log.Fatalf("Failed reading the request body %s", err)
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
-	}
-	log.Printf("this is yout cat %#v", cat)
-	return c.String(http.StatusOK, "We got your Cat!!!")
-}
-func serverHeader(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		c.Response().Header().Set("Custom-Header", "blah!!!")
-		return next(c)
-	}
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
